34d5de326a5cdb47a907fab7ca10737fbfe6d0b9
viillen <yasmimkalfeltz@gmail.com>
2024-03-12 13:45:03 -0300

    signup

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
+    baseUrl: 'https://notes-serverless-app.com',
+    defaultCommandTimeout: 20000,
+    setupNodeEvents(on, config) {
+    },
   },
 })
